6cd71426cb25f273468e7fa1abd9a313e831667b
Marcilio Correia <msc_marcilio@hotmail.com>
2020-11-27 10:53:42 +0000

    TodoListReact

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+    return (
+        <React.Fragement>
+            <h1>About</h1>
+            <p>This is the TodoList app. It is part of the ReactJS Crash Course</p>            
+        </React.Fragement>
+    )
+}
+
+export default About;
